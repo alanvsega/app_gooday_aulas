b4ac22310199bd72c77bb40170654fd30223093c
Alan Vinicius Sega <alanvsega@gmail.com>
2024-05-10 21:30:22 -0300

    inicio de componente de botao

---
+++ lib/src/componets/my_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'color_style.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  final double _buttonProportion;
+  final double _marginSize;
+  final String label;
+  final bool _isPrimary;
+  final VoidCallback onPressedButton;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
